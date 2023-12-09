8081028a5676231a60a6bcb0708c9cbe2733d50c
henrispriet <henri.spriet@gmail.com>
2023-12-09 12:02:03 +0100

    day-08 part1 parser

---
+++ day-08/src/bin/part1.rs
@@ -0,0 +1,182 @@
+use nom::{
+    character::complete::{alpha1, anychar, line_ending},
+    multi::{fold_many1, many1},
+    sequence::{delimited, separated_pair},
+    Parser, combinator::opt,
+};
+use nom_supreme::{error::ErrorTree, tag::complete::tag, ParserExt};
+use std::{collections::HashMap, str::FromStr};
+
+fn process(_input: ParsedData) -> String {
+    todo!()
+}
+
+#[derive(Debug)]
+enum Direction {
+    Left,
+    Right,
+}
+
+impl TryFrom<char> for Direction {
+    type Error = anyhow::Error;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'L' => Ok(Direction::Left),
+            'R' => Ok(Direction::Right),
+            c => Err(anyhow::anyhow!("invalid char: {c}")),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Instructions(Vec<Direction>);
+
+#[derive(Debug, Default)]
+struct Map(HashMap<Node, Junction>);
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Node([char; 3]);
+
+impl FromStr for Node {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        anyhow::ensure!(s.chars().count() == 3, "wrong len: {}", s.len());
+
+        let mut out = ['\0'; 3];
+        let mut chars = s.chars();
+        out[0] = chars.next().expect("len 3");
+        out[1] = chars.next().expect("len 3");
+        out[2] = chars.next().expect("len 3");
+
+        Ok(Node(out))
+    }
+}
+
+#[derive(Debug)]
+struct Junction {
+    left: Node,
+    right: Node,
+}
+
+type ParsedData = (Instructions, Map);
+
+fn parse(input: &str) -> ParsedData {
+    let instructions = many1(anychar::<_, ErrorTree<&str>>.map_res(Direction::try_from))
+        .terminated(line_ending)
+        .map(Instructions);
+    let junction = separated_pair(
+        alpha1.map_res(Node::from_str),
+        tag(" = "),
+        delimited(
+            tag("("),
+            separated_pair(
+                alpha1.map_res(Node::from_str),
+                tag(", "),
+                alpha1.map_res(Node::from_str),
+            ),
+            tag(")"),
+        ),
+    );
+    let parse_map = fold_many1(junction.terminated(opt(line_ending)), Map::default, |mut acc, (from, (left, right))| {
+        acc.0.insert(from, Junction { left, right });
+        acc
+    });
+    let mut parser = separated_pair(instructions, line_ending, parse_map);
+
+    match parser(input) {
+        Ok(("", output)) => output,
+        Ok(output) => panic!(
+            "parsing INCOMPLETE!
+{output:#?}"
+        ),
+        Err(error) => panic!(
+            "parser FAILED!
+{error:#?}"
+        ),
+    }
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    let parsed = parse(input);
+    let output = process(parsed);
+    println!("{output}");
+}
+
+#[test]
+fn parse_example1() {
+    let example_input = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+    parse(example_input);
+}
+
+#[test]
+fn parse_example2() {
+    let example_input = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+    parse(example_input);
+}
+
+#[test]
+fn parse_real_input() {
+    let real_input = include_str!("input.txt");
+    parse(real_input);
+}
+
+#[ignore = "not done with parse"]
+#[test]
+fn example1() {
+    let input = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+    let expected = "2";
+
+    let parsed = parse(input);
+    let output = process(parsed);
+    assert_eq!(expected, output);
+}
+
+#[ignore = "not done with parse"]
+#[test]
+fn example2() {
+    let input = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+    let expected = "6";
+
+    let parsed = parse(input);
+    let output = process(parsed);
+    assert_eq!(expected, output);
+}
+
+#[ignore = "not done with process"]
+#[test]
+fn real_input() {
+    let input = include_str!("input.txt");
+    let expected = "";
+
+    let parsed = parse(input);
+    let output = process(parsed);
+    assert_eq!(expected, output);
+}
