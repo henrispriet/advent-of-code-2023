d8d195bc2c0df1a949a31685dd6afb823a56dce9
henrispriet <henri.spriet@gmail.com>
2024-06-10 17:40:36 +0200

    day-03 add test case and fix

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -20,6 +20,7 @@ fn process(input: &str) -> String {
     for (y, line) in input.lines().enumerate() {
         numbers.first_in_line.push(numbers.numbers.len());
 
+        let line_len = line.chars().count();
         for (x, char) in line.char_indices() {
             if char == '.' {
                 new_number = true;
@@ -40,24 +41,38 @@ fn process(input: &str) -> String {
                     last_digit_x: x,
                     // y,
                 });
+
                 new_number = false;
             } else {
                 let last_number = numbers
                     .numbers
                     .last_mut()
                     .expect("new_number == false => at least one number");
+
                 last_number.value = last_number.value * 10 + digit;
                 last_number.last_digit_x = x;
+            }
+
+            // make sure
+            // .......123
+            // 456.......
+            // is considered as 2 numbers
+            if x == line_len - 1 {
+                new_number = true;
             }
         }
     }
     numbers.first_in_line.push(numbers.numbers.len());
+    dbg!(&numbers.numbers);
 
     // NOTE: assuming a number is next to at most one symbol => map every symbol to potentially multiple numbers
     let valid_numbers = symbols.into_iter().flat_map(|(x, y)| {
-        let relevant_numbers_by_line =
+        let relevant_numbers_by_line = {
+            let start_line_above = numbers.first_in_line[y - 1];
+            let end_line_below = numbers.first_in_line[y + 2];
             // NOTE: assuming no symbols on the first or last line
-            numbers.numbers[numbers.first_in_line[y - 1]..numbers.first_in_line[y + 2]].into_iter();
+            numbers.numbers[start_line_above..end_line_below].iter()
+        };
         relevant_numbers_by_line.filter_map(move |number| {
             (number.last_digit_x >= x - 1 && number.first_digit_x <= x + 1).then_some(number.value)
         })
@@ -76,7 +91,9 @@ fn process(input: &str) -> String {
     //         .then_some(number.value)
     // });
 
-    valid_numbers.sum::<u32>().to_string()
+    dbg!(valid_numbers.collect::<Vec<_>>().iter())
+        .sum::<u32>()
+        .to_string()
 }
 
 fn main() {
@@ -98,6 +115,22 @@ fn example() {
 ...$.*....
 .664.598..";
     let expected = "4361";
+    assert_eq!(expected, process(input));
+}
+
+#[test]
+fn example2() {
+    let input = "467..114..
+...*......
+..35..633.
+......#111
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+    let expected = "4472";
     assert_eq!(expected, process(input));
 }
 
